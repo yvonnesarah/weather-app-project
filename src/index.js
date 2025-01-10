7ac4c91c8f8b081bf0f8fd93fc8e046a54f87ea3
Yvonne Adedeji <yvonne.sarah.adedeji@hotmail.com>
2025-01-10 20:46:29 +0000

    added search city the user entered in the search box

--- src/index.js
+++ src/index.js
@@ -90,4 +90,4 @@ function displayForecast(response) {
 let searchFormElement = document.querySelector('#search-form');
 searchFormElement.addEventListener('submit', handleSearchSubmit);
 
-searchCity('');
+searchCity('London');
